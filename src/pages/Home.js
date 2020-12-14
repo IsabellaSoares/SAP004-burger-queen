88a2f24c7d72d11809b6ee80fe7e7dfb4fce8068
IsabellaSoares <isabella.soares.28@gmail.com>
2020-12-13 22:47:09 -0300

    fix: remove getData from home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useHistory } from 'react-router-dom'
-
-import { getData } from '../services/FirebaseService'
 
 import Button from '../components/Button'
 
 const Home = () => {
   let history = useHistory()
-
-  useEffect(() => {
-    console.log(getData('users', 'email', 'asc'))
-  })
 
   return (
     <>
