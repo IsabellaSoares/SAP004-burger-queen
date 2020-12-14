8b3d27073691f97c86acf8b09e7d68aa3b96c5f1
IsabellaSoares <isabella.soares.28@gmail.com>
2020-12-13 22:12:37 -0300

    fix: set input as required

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Input = ({ type, placeholder, onChange }) => {
   return (
     <input
+      required
       type={type}
       placeholder={placeholder}
       onChange={onChange}
