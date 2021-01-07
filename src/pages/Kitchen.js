e671271bc1e6a0e4f03406f8a59f23daea140438
IsabellaSoares <isabella.soares.28@gmail.com>
2021-01-07 10:26:42 -0300

    get orders

--- src/pages/Kitchen.js
+++ src/pages/Kitchen.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { getOrders } from '../services/FirebaseService'
 
 const Kitchen = () => {
-  return <h1>Kitchen Page</h1>
+  const [orders, setOrders] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    getOrders().then(results => {
+      setOrders(results)
+      setLoading(false)
+    })
+  }, [])
+
+  return (
+    <>
+      <h1>Kitchen Page</h1>
+      <h3>Pedidos</h3>
+
+      {
+        loading ? <h3>Carregando pedidos...</h3> : orders.map(order => <p key={order.date}>{order.name}</p>) 
+      }
+    </>
+  )
 }
 
 export default Kitchen
